417a438ed0b815fac39040f7f10c16d5b16e7a5a
nmvdw <nnmvdw@gmail.com>
2022-10-19 17:16:53 +0200

    hi liye

--- Code/Examples/Map.v
+++ Code/Examples/Map.v
@@ -36,13 +36,17 @@ Definition Nil
 
 Definition Cons
            {C : con base_types}
-  : tm map_ar C (Btype ⟶ List ⟶ List)
-  := BaseTm TCons.
+           (x : tm map_ar C Btype)
+           (xs : tm map_ar C List)
+  : tm map_ar C List
+  := BaseTm TCons · x · xs.
 
 Definition Map
            {C : con base_types}
-  : tm map_ar C ((Btype ⟶ Btype) ⟶ List ⟶ List)
-  := BaseTm TMap.
+           (f : tm map_ar C (Btype ⟶ Btype))
+           (xs : tm map_ar C List)
+  : tm map_ar C List
+  := BaseTm TMap · f · xs.
 
 Definition map_nil
   : rewriteRule map_ar
@@ -50,7 +54,7 @@ Definition map_nil
        ((Btype ⟶ Btype) ,, ∙)
        List
        (let f := TmVar Vz in
-        Map · f · Nil)
+        Map f Nil)
        Nil.
 
 Definition map_cons
@@ -61,11 +65,11 @@ Definition map_cons
        (let f := TmVar Vz in
         let x := TmVar (Vs Vz) in
         let xs := TmVar (Vs (Vs Vz)) in
-        Map · f · (Cons · x · xs))
+        Map f (Cons x xs))
        (let f := TmVar Vz in
         let x := TmVar (Vs Vz) in
         let xs := TmVar (Vs (Vs Vz)) in
-        Cons · (f · x) · (Map · f · xs)).
+        Cons (f · x) (Map f xs)).
 
 Definition map_afs
   : afs base_types fun_symbols
@@ -98,6 +102,15 @@ Definition map_fun_poly
               +P (P_const 3) *P (from_poly y0) *P from_poly (G1 ·P P_base (from_poly y0)))
      end.
 
+Definition koe
+           {n1 n2 m1 m2 : nat}
+           (p : n1 = n2)
+           (q : n2 > m2)
+           (r : m1 = m2)
+  : n1 > m1.
+nia.
+Qed.
+
 Definition map_isSN
   : isSN map_afs.
 Proof.
@@ -111,6 +124,106 @@ Proof.
     + rewrite !PeanoNat.Nat.mul_0_l.
       rewrite !plus_O_n.
       rewrite <- !plus_n_O.
+      induction x as [ f [ y0 [ y1 [] ]]].
+      cbn -[Nat.add Nat.mul].
+
+      (*
+      assert (f (3 + y0 + (3 + y0) + y1 + (y1 + (3 + y0) + (3 + y1 + y0))) >= f y1).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      assert (f (3 + y0 + (3 + y0) + y1 + (y1 + (3 + y0) + (3 + y1 + y0))) >= f y0).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      assert (f (3 + y0 + (3 + y0) + y1 + (y1 + (3 + y0) + (3 + y1 + y0))) >= f 0).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      nia.
+       *)
+      
+      assert (f(4*y0 + 3*y1 + 12) >= f y1).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      assert (f(4*y0 + 3*y1 + 12) >= f y0).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      assert (f(4*y0 + 3*y1 + 12) >= f 0).
+      {
+        apply f.
+        cbn.
+        nia.
+      }
+      
+      enough (36 * f(4*y0 + 3*y1 + 12)
+              + 9 * y1 * f(4*y0 + 3*y1 + 12)
+              + 12 * y0 * f(4*y0 + 3*y1 + 12)
+              + f(4*y0 + 3*y1 + 12)
+              + 3*(f 0)
+              + 20*y0
+              + 15*y1
+              + 72
+              >
+              9*y1*f(y1)
+              + 4*(f y0)
+              + 3*(f y1)
+              + 13*(f 0)
+              + 4*y0
+              + 15*y1
+              + 48).
+      {
+        refine (koe _ H2 _) ; nia.
+
+      }
+      nia.
+      
+      nia.
+        - nia.
+        apply koe.
+        Search (_ > _).
+        admit.
+      }
+      nia.
+        nia.
+
+      assert (f (3 + y0 + (3 + y0) + y1 + (y1 + (3 + y0) + (3 + y1 + y0))) >= f y0).
+      {
+        apply f ; cbn.
+        nia.
+      }
+      assert (f (3 + y0 + (3 + y0) + y1 + (y1 + (3 + y0) + (3 + y1 + y0))) >= f y1).
+      {
+        apply f ; cbn.
+        nia.
+      }
+      assert (forall (z : nat), f z >= f 0).
+      {
+        intro.
+        apply f ; cbn.
+        nia.
+      }
+      nia.
+      
+      f y0
+        f y0
+        nia.
+      cbn.
+      Check fst x.
+      Check snd x.
+      rewrite !plus_O_n.
       admit.
     + destruct Hr.
 Admitted.
